e7a905ba667b77918bc8c0c308f632559e803a70
Punn Nararatwong <punnclub@gmail.com>
2016-08-24 08:34:22 +0700

    Edit todo

--- lesson/week4/js/main.js
+++ lesson/week4/js/main.js
@@ -7,35 +7,39 @@ $(document).ready(function(){
   var $task = $('.task')
   var $btn = $('.btn')
 
-  $delete.click(function Remove(){
+  /*$delete.click(function Remove(){
     //console.log($(this),$(this).parent())
     $(this).parent().remove() //empty() content หาย - remove() tag หายด้วย
-  })
+  })*/
 
   $input.keyup(function(e){
     //console.log($input.val())
     if(e.keyCode == 13)
     {
-      var temp = '<li><input type="checkbox" class="check" /> ' + '<span class="task">' + $input.val() + '</span><span class="delete"> (x)</span></li>'
+      var temp = '<li><input type="checkbox" class="check" /> ' + '<span class="task">' + $input.val() + '</span><span class="delete"> [x]</span></li>'
       $content.append(temp) //prepend วางบน - append วางล่าง
+      $input.val('');
     }
   })
 
   $content.on('click','.delete',function(){
-    $(this).parent().remove()
+    $(this).parent().fadeToggle(300)
   })
 
   $content.on('change','.check',function(){
     if(this.checked){
       $(this).closest('li').find('.task').css("text-decoration", "line-through")
+      $(this).closest('li').find('.task').css("opacity", "0.5")
+      $(this).closest('li').find('.task').css("transition", "opacity 0.3s ease-in-out")
     }else{
       $(this).closest('li').find('.task').css("text-decoration", "none")
+      $(this).closest('li').find('.task').css("opacity", "1")
     }
   })
 
   $btn.click(function(){
     $("input[type=checkbox]:checked").each(function() {
-        $(this).parent().remove()
+        $(this).parent().fadeToggle(300)
     });
   });
 
